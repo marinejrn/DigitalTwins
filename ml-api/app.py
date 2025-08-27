a887dc6da833f92ce9c2334820968c437c9c0c35
Marine <marine.journu@edu.devinci.fr>
2025-08-27 13:56:04 +0200

    ajout port2

--- ml-api/app.py
+++ ml-api/app.py
@@ -32,7 +32,6 @@ def predict():
     })
 
 if __name__ == "__main__":
-    app.run(debug=True)
     port = int(os.environ.get("PORT", 5000))  # Render injecte le port ici
     app.run(host="0.0.0.0", port=port, debug=False)
 
